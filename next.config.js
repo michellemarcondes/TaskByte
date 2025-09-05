49107907840e20487000f81d7c4ad53325688f47
michellemarcondes <mpinheiromarcondes>
2025-09-05 20:14:56 -0300

    agora vai2

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/TaskByte',
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  export default nextConfig;
-  
+  output: 'export',
+  basePath: '/TaskByte', // troque pelo nome do seu repositório no GitHub
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
