3e913942d0ff5e0f5d2190dbe15d3408cd09aa7f
michellemarcondes <mpinheiromarcondes>
2025-09-05 20:08:00 -0300

    ok

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: '/TaskByte',
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  export default nextConfig;
+  
