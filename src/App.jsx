308acf4a0bcea710649ab65617ad8a6b62b79b72
michellemarcondes <mpinheiromarcondes@gmail.com>
2025-09-09 15:44:01 -0300

    okkk

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-200 to-pink-200">
+      <h1 className="text-4xl font-bold text-purple-800">🚀 TaskByte rodando no GitHub Pages!</h1>
+    </div>
+  )
+}
+
+export default App
+
+
